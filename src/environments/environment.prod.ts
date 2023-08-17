d23acb4b631630749bca667cb018d77e229e2d07
James <james3roberts@gmail.com>
2023-08-16 20:23:39 -0600

    Pushing the last of the commits

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  api_url: 'https://q-a-example-loopback-api.herokuapp.com'
+  //api_url: 'https://q-a-example-loopback-api.herokuapp.com'
+  api_url: 'http://anotheronecauseimessedup.s3-website.us-east-2.amazonaws.com'
 };
